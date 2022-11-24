3a620b3c69059019abf26a91d347aba9c2a75906
hathaichanok1089 <112158508+hathaichanok1089@users.noreply.github.com>
2022-11-24 22:35:52 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        bool isEnd = false;
+        Queue<char> queue = new Queue<char>();
+        while (!isEnd)
+        {
+            Console.Write("Enter the size:");
+            char input = Convert.ToChar(Console.ReadLine());
+            if(input != 'S' && input != 'M' && input != 'L')
+            {
+                isEnd =true;
+            }
+            else
+            {
+                queue.Push(input);
+            }
+        }
+
+        while (queue.GetLength() != 0)
+        {
+            char x = queue.Pop();
+
+            if(x == 'L')
+            {
+                queue.Push('M');
+                queue.Push('M');
+                Console.Write('1');
+            }
+            else if (x == 'M')
+            {
+                queue.Push('S');
+                queue.Push('S');
+                queue.Push('S');
+                Console.Write('2');
+            }
+            else if (x == 'S')
+            {
+                Console.Write('3');
+            }
+        }
+
+    }
+}
